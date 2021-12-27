801f2f79e8e5965ad28942366fa80f765cf39237
wielorybek <63021070+wielorybek@users.noreply.github.com>
2021-12-27 01:16:39 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace GPS_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLocationData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textCOM = new System.Windows.Forms.TextBox();
+            this.textWidth = new System.Windows.Forms.TextBox();
+            this.textLength = new System.Windows.Forms.TextBox();
+            this.buttonShowMap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLocationData
+            // 
+            this.buttonLocationData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.buttonLocationData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLocationData.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonLocationData.Location = new System.Drawing.Point(66, 280);
+            this.buttonLocationData.Name = "buttonLocationData";
+            this.buttonLocationData.Size = new System.Drawing.Size(263, 92);
+            this.buttonLocationData.TabIndex = 0;
+            this.buttonLocationData.Text = "Show location data";
+            this.buttonLocationData.UseVisualStyleBackColor = false;
+            this.buttonLocationData.Click += new System.EventHandler(this.buttonLocationData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(66, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(66, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(66, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Length";
+            // 
+            // textCOM
+            // 
+            this.textCOM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.textCOM.ForeColor = System.Drawing.SystemColors.Control;
+            this.textCOM.Location = new System.Drawing.Point(172, 49);
+            this.textCOM.Name = "textCOM";
+            this.textCOM.Size = new System.Drawing.Size(157, 27);
+            this.textCOM.TabIndex = 4;
+            // 
+            // textWidth
+            // 
+            this.textWidth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.textWidth.ForeColor = System.Drawing.SystemColors.Control;
+            this.textWidth.Location = new System.Drawing.Point(172, 112);
+            this.textWidth.Name = "textWidth";
+            this.textWidth.Size = new System.Drawing.Size(157, 27);
+            this.textWidth.TabIndex = 5;
+            // 
+            // textLength
+            // 
+            this.textLength.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.textLength.ForeColor = System.Drawing.SystemColors.Control;
+            this.textLength.Location = new System.Drawing.Point(172, 173);
+            this.textLength.Name = "textLength";
+            this.textLength.Size = new System.Drawing.Size(157, 27);
+            this.textLength.TabIndex = 6;
+            // 
+            // buttonShowMap
+            // 
+            this.buttonShowMap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(34)))));
+            this.buttonShowMap.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonShowMap.Location = new System.Drawing.Point(423, 51);
+            this.buttonShowMap.Name = "buttonShowMap";
+            this.buttonShowMap.Size = new System.Drawing.Size(276, 229);
+            this.buttonShowMap.TabIndex = 7;
+            this.buttonShowMap.Text = "Show on map";
+            this.buttonShowMap.UseVisualStyleBackColor = false;
+            this.buttonShowMap.Click += new System.EventHandler(this.buttonShowMap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonShowMap);
+            this.Controls.Add(this.textLength);
+            this.Controls.Add(this.textWidth);
+            this.Controls.Add(this.textCOM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLocationData);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "GPS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLocationData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textCOM;
+        private System.Windows.Forms.TextBox textWidth;
+        private System.Windows.Forms.TextBox textLength;
+        private System.Windows.Forms.Button buttonShowMap;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Globalization;
+
+namespace GPS_1
+{
+    public partial class Form1 : Form
+    {
+
+        string width;
+        string length;
+        string data;
+        SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenPort(string com)
+        {
+            try
+            {
+                serialPort = new SerialPort(com, 9600, Parity.None, 8, StopBits.One);
+                serialPort.Open();
+                //serialPort.DataReceived += new SerialDataReceivedEventHandler(PortDataReceived);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.StackTrace + " " + e.Message, "Port error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void PortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort.ReadExisting();
+        }
+
+        private void ReadPort()
+        {
+            if (serialPort.IsOpen)
+            {
+                Thread.Sleep(2);
+                string data = serialPort.ReadExisting();
+                string[] lines = data.Split('$');
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split(',');
+                    if (words[0] == "GPGGA")
+                    {
+                        
+                        try
+                        {
+                            //Double width = Convert.ToDouble(words[2]);
+                            string widthString = words[2];
+                            string col1 = words[3];
+                            //width = width / 100;
+                            //string[] widthElements = width.ToString().Split('.');
+                            //this.width = words[3] + widthElements[0] + "." + ((Convert.ToDouble(widthElements[1]) / 60)).ToString("#####");
+                            //this.width = words[3] + width.ToString();
+
+                            //Double length = Convert.ToDouble(words[4]);
+                            string lengthString = words[4];
+                            string col2 = words[5];
+
+                            string heightString = words[9];
+                            //length = length / 100;
+                            //this.length = words[5] + length.ToString();
+                            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-GB");
+
+                            double width = Double.Parse(widthString, culture);
+                            double length = Double.Parse(lengthString, culture);
+                            double height = Double.Parse(heightString, culture);
+                            width /= 100;
+                            length /= 100;
+
+                            string wid = width.ToString();
+                            string len = length.ToString();
+                            string wid2 = wid.Replace(',', '.');
+                            string len2 = len.Replace(',', '.');
+
+                            this.width = wid2;
+                            this.length = len2;
+
+                            textWidth.Text = this.width;
+                            textLength.Text = this.length;
+                        }
+                        catch
+                        {
+                            textWidth.Text = "GPS Unavailable";
+                            textLength.Text = "GPS Unavailable";
+                        }
+                    }
+
+                }
+
+            }
+            else
+            {
+                textWidth.Text = "COM Port Closed";
+                textLength.Text = "COM Port Closed";
+            }
+        }
+
+        private void ClosePort()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void ShowOnMap()
+        {
+            try
+            {
+                string result = this.width + "," + this.length;
+                string address = "http://www.google.com/maps/place/";
+
+                if (this.width != "" && this.length != "")
+                {
+                    address += result;
+                }
+
+                OpenURL(address);
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.StackTrace + " " + e2.Message, "Map error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void OpenURL(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                url = url.Replace("&", "^&");
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+        }
+
+        private void buttonLocationData_Click(object sender, EventArgs e)
+        {
+            OpenPort(textCOM.Text);
+            ReadPort();
+            ClosePort();
+        }
+
+        private void buttonShowMap_Click(object sender, EventArgs e)
+        {
+            ShowOnMap();
+        }
+    }
+}
